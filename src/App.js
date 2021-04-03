fab6fc1007eb7f263c62c46138f59fb1ed3041a5
GautamPB <pbg2402@gmail.com>
2021-04-03 23:31:00 +0530

    Added: Axios API fetcher to fetch from backend server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import './App.css'
+import React, { useState, useEffect } from 'react'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+import Pusher from 'pusher-js'
+import axios from './axios'
 
 const App = () => {
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        //fetching the initial information
+        axios.get('/messages/sync').then((response) => {
+            setMessages(response.data)
+        })
+    }, [])
+
+    useEffect(() => {
+        var pusher = new Pusher('b9fea509874c79644820', {
+            cluster: 'ap2',
+        })
+
+        var channel = pusher.subscribe('messages')
+        channel.bind('inserted', (newMessage) => {
+            alert(JSON.stringify(newMessage))
+            setMessages([...messages, newMessage])
+        })
+
+        return () => {
+            channel.unbind_all()
+            channel.unsubscribe()
+        }
+    }, [messages])
+
+    console.log(messages)
+
     return (
         <div className="app">
             <div className="app__body">
